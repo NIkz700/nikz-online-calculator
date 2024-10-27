5f6e931340c03edee048750bf35d62e39b00fe37
NOOB JAY <nikosedan94@gmail.com>
2024-10-27 08:03:48 +0800

    Add files via upload

---
+++ styles.js
@@ -0,0 +1,11 @@
+function appendValue(value) {
+    document.getElementById("display").value += value;
+}
+
+function clearDisplay() {
+    document.getElementById("display").value = "";
+}
+
+function showLove() {
+    document.getElementById("display").value = "I LOVE YOU🥺";
+}
